2526b4d3489b3d099a85e7a968d96d30d455e7b7
jacob washington <jacobwashington.dev@gmail.com>
2021-06-16 12:03:52 -0700

    [Education] add education experience

---
+++ src/EducationData.js
@@ -0,0 +1,25 @@
+import SCC from "./images/logos/SCClogo.svg";
+import GA from "./images/logos/GAlogo.svg";
+
+const EducationData = [
+  {
+    role: "Software Engineering Immersive",
+    employer: "General Assembly",
+    years: "2021",
+    logo: GA,
+    bullets: [
+      "Full-stack software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developed a portfolio of individual and group projects",
+    ],
+  },
+  {
+    role: "Sociology & Anthropology",
+    employer: "Seattle Central College",
+    years: "2016 - 2018",
+    logo: SCC,
+    bullets: [
+      "As a student of sociology at Seattle Central College, I took my learning from pages to practice. Theories and concepts I’d discovered—both in the classroom and in my own studies—greatly assisted my development as a leader in the BSU and my ability to lead the BSU in coalescing with organizations with similar goals",
+    ],
+  },
+];
+
+export default EducationData;
--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import "./Education.css"
+import EducationData from "../../EducationData";
 import Experience from "../../components/experience/Experience";
-import USArmy from "../../images/logos/us-army.svg"
 
 const Education = () => {
-    let list = [
-        "Led and mentored new recruits as they entered the 14th Engineer Battalion",
-        "Devised a training plan and constructed training resources for soldiers using mine detection equipment in support of Operation Enduring Freedom",
-        "Served as an integral member of a team, successfully conducting over 3000 hours of combat operations in support of Operation Enduring Freedom"
-    ]
+
+    let mappedExperience = EducationData.map((exp, index) => {
+        return (
+            <Experience
+            logo={exp.logo}
+            role={exp.role}
+            company={exp.employer}
+            bullets={exp.bullets}
+            key={index}
+          />
+        )
+    })
+
     return (
         <div className="edu-container">
-            <Experience logo={USArmy} role="Combat Engineer" company="US Army" bullets={list}/>
+            {mappedExperience}
         </div>
     );
 }
